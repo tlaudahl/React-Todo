4663e59dc6243635fcb9afffa6495d197277d425
tlaudahl <travis.laudahl@gmail.com>
2019-09-30 19:46:25 -0500

    MVP and Stretch done

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,18 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react'
+import Todo from './Todo'
+
+export default function TodoList(props) {
+    return (
+        <div className='todoListContainer'>
+            <h2>Todo List</h2>
+            <div className='todoList'>
+                {props.items.map(item => (
+                    <Todo key={item.id} item={item} toggleItem={props.toggleItem} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
